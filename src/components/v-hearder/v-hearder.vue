0cfda4c9e20bee87ae454f97120073a89abf9b5c
heshijia <1634943446@qq.com>
2019-08-11 12:11:00 +0800

    完成头部详情，初始化tab组件

--- src/components/v-hearder/v-hearder.vue
+++ src/components/v-hearder/v-hearder.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header">
+  <div class="header" @click="showDetail">
     <div class="content-wrapper">
       <div class="avatar">
         <img width="64" height="64" :src="seller.avatar">
@@ -34,17 +34,26 @@
 
 <script>
   import SupportIco from 'components/support-ico/support-ico'
+
   export default {
     name: 'v-header',
     props: {
       seller: {
         type: Object,
-        default() {
+        default () {
           return {}
         }
       }
     },
     methods: {
+      showDetail () {
+        this.headerDetailCom = this.headerDetailCom || this.$createHeaderDetail({
+          $props: {
+            seller: 'seller'
+          }
+        })
+        this.headerDetailCom.show()
+      }
     },
     components: {
       SupportIco
@@ -61,42 +70,53 @@
     overflow: hidden
     color: $color-white
     background: $color-background-ss
+
     .content-wrapper
       position: relative
       display: flex
       align-items: center
       padding: 24px 12px 18px 24px
+
       .avatar
         flex: 0 0 64px
         width: 64px
         margin-right: 16px
+
         img
           border-radius: 2px
+
       .content
         flex: 1
+
         .title
           display: flex
           align-items: center
           margin-bottom: 8px
+
           .brand
             width: 30px
             height: 18px
             bg-image('brand')
             background-size: 30px 18px
             background-repeat: no-repeat
+
           .name
             margin-left: 6px
             font-size: $fontsize-large
             font-weight: bold
+
         .description
           margin-bottom: 8px
           line-height: 12px
           font-size: $fontsize-small
+
         .support
           display: flex
           align-items: center
+
           .support-ico
             margin-right: 4px
+
           .text
             line-height: 12px
             font-size: $fontsize-small-s
@@ -113,8 +133,10 @@
         text-align: center
         border-radius: 14px
         background: $color-background-sss
+
         .count
           font-size: $fontsize-small-s
+
         .icon-keyboard_arrow_right
           margin-left: 2px
           line-height: 24px
@@ -128,6 +150,7 @@
       line-height: 28px
       padding: 0 8px
       background: $color-background-sss
+
       .bulletin-title
         flex: 0 0 22px
         width: 22px
@@ -136,16 +159,19 @@
         bg-image('bulletin')
         background-size: 22px 12px
         background-repeat: no-repeat
+
       .bulletin-text
         flex: 1
         white-space: nowrap
         overflow: hidden
         text-overflow: ellipsis
         font-size: $fontsize-small-s
+
       .icon-keyboard_arrow_right
         flex: 0 0 10px
         width: 10px
         font-size: $fontsize-small-s
+
     .background
       position: absolute
       top: 0
